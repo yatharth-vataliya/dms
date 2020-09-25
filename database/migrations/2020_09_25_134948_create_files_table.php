86f79295b046d5cd40c6ea8eef18e92b6fa8d9b2
yatharth-vataliya <yatharthvataliya@gmail.com>
2020-09-25 19:22:32 +0530

    migration, model, factory, seeder and controller added successfully.

---
+++ database/migrations/2020_09_25_134948_create_files_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->id();
+            $table->string('file_name');
+            $table->bigInteger('file_size');
+            $table->enum('status',['active','inactive'])->default('active');
+            $table->timestamps();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+}
