ba81cf5073e381cf67b4843fe5f70285f0b21eb5
yatharth-vataliya <yatharthvataliya@gmail.com>
2020-10-18 21:24:37 +0530

    directory create and show with rewind option is done now

--- app/Http/Livewire/FileHandling.php
+++ app/Http/Livewire/FileHandling.php
@@ -10,20 +10,24 @@ use App\Models\File;
 
 class FileHandling extends Component
 {
-    use WithFileUploads,WithPagination;
+    use WithFileUploads, WithPagination;
 
     public $documents = [];
-    public string $search = '';
+    public $search = '';
+    public $folder_name = '';
+    public $dir = '';
+    public $directories = '';
+    public $all_files = '';
 
-    public function updatedDocuments()
+    public function updated($property)
     {
-        $this->validate([
-            'documents.*' => 'required|max:512000|mimes:sql,SQL,jpg,jpeg,JPG,JPEG,png,PNG,xlsx,doc,docx,zip,pdf,csv,mp4,mp3,mkv,avi,gif,exe,deb,tar,3gp'
+        $this->validateOnly($property, [
+            'folder_name' => 'required|string|max:50|regex:/^[a-zA-Z]+$/'
         ], [
-            'documents.*.mimes' => 'The :attribute have no valid type please upload valid file type.',
-            'documents.*.max' => 'The :attribute max size is out of range please make sure file size is below 500 MB'
+            'folder_name.required' => 'Please enter :attribute',
+            'folder_name.regex' => 'Please enter valid :attribute'
         ], [
-            'documents.*' => 'Uploaded File'
+            'folder_name' => 'Folder Name'
         ]);
     }
 
@@ -40,13 +44,27 @@ class FileHandling extends Component
         foreach ($this->documents as $document):
             $file_name = time() . mt_rand(10000, 99999);
             $file_name .= $document->getClientOriginalName();
-            $db_save_path = 'user_documents/user_' . auth()->user()->id . "/" . $file_name;
+//            $db_save_path = 'user_documents/user_' . auth()->user()->id . "/" . $file_name;
+            $db_save_path = $this->dir . $file_name;
             $save_path = 'public/user_documents/user_' . auth()->user()->id . "/";
             // Storage::disk('public')->putFileAs($document,$path,$file_name);
             // $document->move($path,$file_name);
             // Storage::move($document,$path);
-            $document->storeAs($save_path, $file_name);
-            // $file_size = ceil((filesize(storage_path('app/public'.$db_save_path))/1024/1024));
+//            $document->storeAs($save_path, $file_name);
+            /*if(!file_exists($this->dir)){
+                mkdir($this->dir,0777,true);
+            }*/
+            $uploaded_file_size = $document->getSize() / 1024 / 1024;
+            $storage_limit = auth()->user()->storage_limit * 1024;
+            $used_space = File::where('user_id', auth()->user()->id)->get()->sum('file_size');
+            $uploaded_file_size += $used_space;
+
+
+            if ($uploaded_file_size > $storage_limit) {
+                session()->flash('info', 'Sorry Your Storage Limit is going to out of range.');
+                return redirect()->back();
+            }
+            $document->storeAs($this->dir, $file_name);
             File::create([
                 'file_name' => $document->getClientOriginalName(),
                 'file_url' => $db_save_path,
@@ -56,17 +74,57 @@ class FileHandling extends Component
         endforeach;
         $this->documents = [];
         $this->dispatchBrowserEvent('reset-form');
+        $this->directories = Storage::directories($this->dir);
+        $this->all_files = Storage::files($this->dir);
         session()->flash('info', 'Your files are uploaded successfully.');
     }
 
-    public function updatingSearch(){
+    public function updatingSearch()
+    {
         $this->resetPage();
+    }
+
+    public function createFolder()
+    {
+        Storage::makeDirectory($this->dir . '/' . $this->folder_name);
+        $this->folder_name = '';
+        $this->directories = Storage::directories($this->dir);
+        $this->all_files = Storage::files($this->dir);
+        session()->flash('info', 'Folder Created Successfully.');
+    }
+
+    public function downloadFile($filename = NULL)
+    {
+        return Storage::download($filename);
+    }
+
+    public function changeDir($dir = NULL)
+    {
+        $this->dir = decrypt($dir) . '/';
+        $this->directories = Storage::directories($this->dir);
+        $this->all_files = Storage::files($this->dir);
+    }
+
+    public function rewind()
+    {
+        $dir_array = explode('/',trim($this->dir,'/'));
+        array_pop($dir_array);
+        $this->dir = implode('/',$dir_array) .'/';
+        $this->directories = Storage::directories($this->dir);
+        $this->all_files = Storage::files($this->dir);
+    }
+
+    public function mount()
+    {
+        $this->dir = 'public/user_documents/user_' . auth()->user()->id . '/';
+        $this->directories = Storage::directories($this->dir);
+        $this->all_files = Storage::files($this->dir);
     }
 
     public function render()
     {
-        $files = File::where('user_id', auth()->user()->id)->where('file_name', 'like', '%'.$this->search.'%')->latest()->paginate(8);
+        $files = File::where('user_id', auth()->user()->id)->where('file_name', 'like', '%' . $this->search . '%')->latest()->paginate(8);
         $used_space = File::where('user_id', auth()->user()->id)->get()->sum('file_size');
-        return view('livewire.file-handling',compact('files','used_space'));
+        return view('livewire.file-handling', compact('files', 'used_space'));
     }
 }
--- resources/views/livewire/file-handling.blade.php
+++ resources/views/livewire/file-handling.blade.php
@@ -14,9 +14,9 @@
             {{ session('info') }}
         </div>
     @endif
-    <div class="text-center">
+    <div class="text-center grid grid-cols-2">
         <form enctype="multipart/form-data" id="user_file_form" wire:submit.prevent="uploadDocument">
-            <input type="file" style="display: none;" id="file_input" wire:model="documents" multiple>
+            <input type="file" style="display: none;" id="file_input" wire:model.defer="documents" multiple>
             <button
                 class="bg-blue-700 rounded p-2 text-lg text-white hover:text-gray-700 hover:bg-white hover:shadow-md focus:outline-none transition duration-300 ease-in-out"
                 type="button" onclick="document.getElementById('file_input').click();">Upload File
@@ -26,16 +26,81 @@
                 Submit
             </button>
         </form>
+        <form wire:submit.prevent="createFolder">
+            <label for="folder_name" class="text-lg">Create Folder </label>
+            <input type="text" name="folder_name" id="folder_name" wire:model.debounce.500ms="folder_name"
+                   class="focus:outline-none focus:shadow-outline-green p-2 rounded bg-green-300 text-black focus:bg-white w-64 placeholder-green-700"
+                   placeholder="Enter Folder Name">
+            <button type="submit"
+                    class="p-2 mr-1 text-lg bg-green-700 text-white hover:bg-white hover:text-black rounded hover:shadow-md focus:outline-none transition duration-300 ease-in-out">
+                Create
+            </button>
+        </form>
     </div>
-    <div class="text-center rounded-full w-68 p-4 m-2 bg-white shadow text-lg text-teal-700 grid grid-cols-2 items-center items-baseline">
+    <div
+        class="text-center rounded-full w-68 p-4 m-2 bg-white shadow text-lg text-teal-700">
         <div>
             {{ $used_space }} MB used out of {{ auth()->user()->storage_limit }} GB
         </div>
-        <div>
-            <input type="text" name="search" id="search" wire:model="search" class="focus:outline-none focus:shadow-outline-green p-2 rounded bg-green-300 text-black focus:bg-white w-64 placeholder-green-700" placeholder="Search File Here">
-        </div>
     </div>
-    <div class="bg-white rounded p-4 grid grid-cols-4">
+    @php
+        $chars = strlen('public/user_documents/user_'.auth()->user()->id);
+    @endphp
+    <div class="p-2 my-2 bg-white shadow-md text-lg grid grid-cols-2">
+        <div>
+            {{ substr($dir,$chars) }}
+        </div>
+        @if(!empty(trim(substr($dir,$chars,),'/')))
+            <div class="w-10 h-10 bg-green-500 rounded-full shadow hover:shadow-md hover:bg-green-400"
+                 wire:click="rewind">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                    <path
+                        d="M8.445 14.832A1 1 0 0010 14v-2.798l5.445 3.63A1 1 0 0017 14V6a1 1 0 00-1.555-.832L10 8.798V6a1 1 0 00-1.555-.832l-6 4a1 1 0 000 1.664l6 4z"/>
+                </svg>
+            </div>
+        @endif
+    </div>
+    <div class="p-4 bg-green-100 mb-2 shadow grid grid-cols-8">
+        @foreach($directories as $dir)
+            <div class="w-28 h-38 rounded hover:bg-white p-2 m-2 hover:shadow-md shadow">
+                <label for="" class="text-center break-words cursor-pointer"
+                       wire:click="changeDir('{{ encrypt($dir) }}')">
+                    @php
+                        $dir_array = explode('/',$dir);
+                        $dir_name = end($dir_array);
+                    @endphp
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                        <path d="M2 6a2 2 0 012-2h5l2 2h5a2 2 0 012 2v6a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"/>
+                    </svg>
+                    <span class="text-green-700">{{ $dir_name }}</span>
+                </label>
+            </div>
+        @endforeach
+        @foreach($all_files as $file)
+            <div class="w-28 h-38 rounded hover:bg-white p-2 m-2 hover:shadow-md shadow truncate">
+                <svg class="svg-icon" viewBox="0 0 20 20">
+                    <path
+                        d="M15.475,6.692l-4.084-4.083C11.32,2.538,11.223,2.5,11.125,2.5h-6c-0.413,0-0.75,0.337-0.75,0.75v13.5c0,0.412,0.337,0.75,0.75,0.75h9.75c0.412,0,0.75-0.338,0.75-0.75V6.94C15.609,6.839,15.554,6.771,15.475,6.692 M11.5,3.779l2.843,2.846H11.5V3.779z M14.875,16.75h-9.75V3.25h5.625V7c0,0.206,0.168,0.375,0.375,0.375h3.75V16.75z"></path>
+                </svg>
+                <label for="" class="text-center break-all">
+                    @php
+                        $file_array = explode('/',$file);
+                        $file_name = end($file_array);
+                    @endphp
+                    {{ $file_name }} <span class="bg-yellow-500 rounded">{{ number_format(\Illuminate\Support\Facades\Storage::size($file) / 1024 /1024,2) }} MB </span>
+                </label>
+                <div class="bg-red-500 shadow p-2 rounded hover:shadow-md">
+                    <span wire:click="downloadFile('{{ $file }}')" class="text-white cursor-pointer">Download</span>
+                </div>
+            </div>
+        @endforeach
+    </div>
+    <div class="p-2 my-2 shadow rounded bg-white">
+        <input type="text" name="search" id="search" wire:model="search"
+               class="focus:outline-none focus:shadow-outline-green p-2 rounded bg-green-300 text-black focus:bg-white w-64 placeholder-green-700"
+               placeholder="Search File Here">
+    </div>
+    <div class="bg-white rounded p-4 grid grid-cols-4 shadow">
         @foreach($files as $file)
             <div class="p-4 m-2 bg-green-200 text-center hover:bg-green-300 hover:shadow-md rounded break-all">
                 <div class="text-lg my-2">
@@ -43,7 +108,9 @@
                 </div>
                 <div class="p-2 bg-gray-300 rounded-md">
                     <div class="grid grid-cols-4">
-                        <a href="{{ asset('storage/'.$file->file_url) }}" download="{{ $file->file_name }}" class="hover:shadow-md rounded hover:bg-white">
+                        <a href="{{ \Illuminate\Support\Facades\Storage::url($file->file_url) }}"
+                           download="{{ $file->file_name }}"
+                           class="hover:shadow-md rounded hover:bg-white">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                                 <path fill-rule="evenodd"
                                       d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3.586l-1.293-1.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V8z"
